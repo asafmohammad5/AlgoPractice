125eaf709df8808dc945a8cbe59cec84f2321de0
asaf <asafmohammad5@gmail.com>
2020-04-27 16:33:06 -0500

    Leetcode problem 1309

--- problems.js
+++ problems.js
@@ -657,5 +657,47 @@ var uniqueMorseRepresentations = function (words) {
   return setN.size
 };
 
+var freqAlphabets = function (s) {
+  const map = {
+    '1': "a",
+    '2': "b",
+    '3': "c",
+    '4': "d",
+    '5': "e",
+    '6': "f",
+    '7': "g",
+    '8': "h",
+    '9': "i",
+    '10#': "j",
+    '11#': "k",
+    '12#': "l",
+    '13#': "m",
+    '14#': "n",
+    '15#': "o",
+    '16#': "p",
+    '17#': "q",
+    '18#': "r",
+    '19#': "s",
+    '20#': "t",
+    '21#': "u",
+    '22#': "v",
+    '23#': "w",
+    '24#': "x",
+    '25#': "y",
+    '26#': "z"
+  }
+  let result = ''
+  for (let i = s.length - 1; i >= 0; i--) {
+    if (s[i] === '#') {
+      const context = s[i - 2] + s[i - 1] + '#'
+      result = map[context] + result
+      i -= 2
+    } else {
+      result = map[s[i]] + result
+    }
+  }
+  return result
+};
+
 
 
