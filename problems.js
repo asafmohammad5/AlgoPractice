e18a1a5fd8dc661f420f1bd7cdd24ae4a6ee0d7f
asaf <asafmohammad5@gmail.com>
2020-04-29 14:08:30 -0500

    Leetcode problem 617

--- problems.js
+++ problems.js
@@ -776,3 +776,12 @@ var selfDividingNumbers = function (left, right) {
   return arr
 };
 
+var mergeTrees = function (t1, t2) {
+  if (t2 === null) return t1;
+  if (t1 === null) return t2;
+  t1.val = t1.val + t2.val;
+  t1.left = mergeTrees(t1.left, t2.left);
+  t1.right = mergeTrees(t1.right, t2.right);
+  return t1
+
+};
