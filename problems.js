aec3c1e141dc7c2fc38f8e99391e9d4d23c49972
asaf <asafmohammad5@gmail.com>
2020-05-22 16:24:29 -0500

    Leetcode problem 1002 workthrough

--- problems.js
+++ problems.js
@@ -1096,3 +1096,20 @@ var helper = function (local) {
   }
   return validLocal;
 };
+
+var commonChars = function (A) {
+  let res = [...A[0]];
+
+  for (let i = 1; i < A.length; i++) {
+    console.log(res)
+    res = res.filter(c => {
+
+      const l = A[i].length;
+      A[i] = A[i].replace(c, "");
+
+      return l > A[i].length;
+    });
+  }
+  return res;
+};
+
