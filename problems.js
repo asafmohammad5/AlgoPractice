d7160e16c42592dea69f1ba7eefb9513244c24f1
asaf <asafmohammad5@gmail.com>
2020-06-25 14:44:43 -0500

    leet code problem 1217

--- problems.js
+++ problems.js
@@ -1506,3 +1506,16 @@ function depth(node) {
   console.log(node.right)
   node.right = temp;
 };
+
+var minCostToMoveChips = function (chips) {
+  let odd = 0;
+  let even = 0;
+  for (let chip of chips) {
+    if (chip % 2 === 0) {
+      even++;
+    } else {
+      odd++;
+    }
+  }
+  return Math.min(even, odd);
+};
