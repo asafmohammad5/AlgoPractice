84410b1e14eb8fae76e68e78e1bf2181835fcbcb
asaf <asafmohammad5@gmail.com>
2020-07-06 14:36:39 -0500

    leet code problem 669

--- problems.js
+++ problems.js
@@ -1638,3 +1638,16 @@ var transpose = function (A) {
   }
   return results;
 };
+
+var trimBST = function (root, L, R) {
+  if (!root) return null;
+  if (root.val < L) {
+    return trimBST(root.right, L, R)
+  } else if (root.val > R) {
+    return trimBST(root.left, L, R)
+  } else {
+    root.left = trimBST(root.left, L, R);
+    root.right = trimBST(root.right, L, R);
+  }
+  return root;
+};
