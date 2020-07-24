0de02af9f1e4be429e0a43b4e39c688ef6d4e84e
asaf <asafmohammad5@gmail.com>
2020-07-24 15:38:22 -0500

    leet code problem 1332

--- problems.js
+++ problems.js
@@ -1964,3 +1964,21 @@ var findSpecialInteger = function (arr) {
   return Object.keys(appear).find(key => appear[key] === num)
 };
 
+var removePalindromeSub = function (s) {
+  if (s.length === 0) {
+    return 0;
+  }
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    if (s[left] === s[right]) {
+      left++;
+      right--;
+    } else {
+      return 2;
+    }
+  }
+  return 1;
+};
+
