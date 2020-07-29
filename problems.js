29ec7dd9bcce1f7bc6bf28663332767b44562f2f
asaf <asafmohammad5@gmail.com>
2020-07-29 16:13:23 -0500

    leet code problem 1379

--- problems.js
+++ problems.js
@@ -2015,3 +2015,12 @@ SubrectangleQueries.prototype.getValue = function (row, col) {
   return this.rectangle[row][col]
 };
 
+var getTargetCopy = function (original, cloned, target) {
+  if (!cloned) return
+  if (cloned.val == target.val) {
+    return cloned
+  } else {
+    return getTargetCopy(original, cloned.left, target) || getTargetCopy(original, cloned.right, target)
+  }
+};
+
