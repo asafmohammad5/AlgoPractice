548781d1aa6d463ad5bdbbeb2fbcafca863d43db
asaf <asafmohammad5@gmail.com>
2020-10-02 17:50:18 -0500

    leet code problem 885

--- problems.js
+++ problems.js
@@ -2827,3 +2827,57 @@ var findDuplicates = function (nums) {
   }
   return results;
 };
+
+
+
+// Difficult to understand leetcode problem, had to look up answer.
+var spiralMatrixIII = function (R, C, r0, c0) {
+  let direction = 'r';
+  const ans = [[r0, c0]];
+  let steps = 1;
+  let moved = 0;
+  let curr = ans[ans.length - 1].slice(0);
+  const N = R * C;
+
+  while (ans.length < N) {
+    const lastStep = steps - 1;
+
+    for (let i = 0; i <= lastStep; i++) {
+      if (direction === 'r') {
+        curr[1]++;
+        if (i === lastStep) {
+          moved++;
+          direction = 'd';
+        }
+      } else if (direction === 'd') {
+        curr[0]++;
+        if (i === lastStep) {
+          moved++;
+          direction = 'l';
+        }
+      } else if (direction === 'l') {
+        curr[1]--;
+        if (i === lastStep) {
+          moved++;
+          direction = 'u';
+        }
+      } else if (direction === 'u') {
+        curr[0] = curr[0] - 1;
+        if (i === lastStep) {
+          moved++;
+          direction = 'r';
+        }
+      }
+
+      if ((curr[1] >= 0 && curr[1] < C) && (curr[0] >= 0 && curr[0] < R)) {
+        ans.push([curr[0], curr[1]]);
+      }
+    }
+
+    if (moved === 2) {
+      steps++;
+      moved = 0;
+    }
+  }
+  return ans;
+};
