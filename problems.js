57f3aa4fd87480e4bac07f1fd2e02ecec240e6df
asaf <asafmohammad5@gmail.com>
2020-10-08 13:41:36 -0500

    leet code problem 419

--- problems.js
+++ problems.js
@@ -2955,7 +2955,38 @@ var pseudoPalindromicPaths = function (root) {
       count++
     }
   }
-
   return count;
-
 };
+
+var countBattleships = function (board) {
+
+  let count = 0;
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[0].length; j++) {
+
+      if (board[i][j] === 'X') {
+        if (i === 0 && j === 0) {
+          count++;
+        }
+        else if (i === 0) {
+          if (board[i][j - 1] === '.') {
+            count++;
+          }
+        }
+        else if (j === 0) {
+          if (board[i - 1][j] === '.') {
+            count++;
+          }
+        }
+        else {
+          if (board[i - 1][j] === '.' && board[i][j - 1] === '.') {
+            count++;
+          }
+        }
+      }
+
+    }
+  }
+  return count;
+}
