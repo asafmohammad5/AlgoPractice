b12de19da73ec0384a034964b2f0a263e2797aba
asaf <asafmohammad5@gmail.com>
2020-11-04 14:03:13 -0600

    leet code problem 1470

--- problems.js
+++ problems.js
@@ -3185,3 +3185,12 @@ var runningSum = function (nums) {
     return sum;
   })
 };
+
+var shuffle = function (nums, n) {
+  let arr = [];
+  for (let i = 0; i < n; i++) {
+    arr.push(nums[i]);
+    arr.push(nums[i + n])
+  };
+  return arr;
+};
