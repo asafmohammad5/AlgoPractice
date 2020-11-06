0d2012c9ac6dfcf57a6ef1b6959ce2da3a5a72ea
asaf <asafmohammad5@gmail.com>
2020-11-06 13:52:29 -0600

    leet code problem 1640

--- problems.js
+++ problems.js
@@ -3194,3 +3194,14 @@ var shuffle = function (nums, n) {
   };
   return arr;
 };
+
+var canFormArray = function (arr, pieces) {
+  let total = "";
+  arr = arr.join("");
+  for (let i = 0; i < pieces.length; i++) {
+    pieces[i] = pieces[i].join("");
+    total += pieces[i];
+    if (arr.indexOf(pieces[i]) == -1) return false;
+  }
+  return total.length == arr.length;
+};
