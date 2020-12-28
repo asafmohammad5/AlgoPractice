99bb603e47b039bb17fde107ca622b3388051a41
asaf <asafmohammad5@gmail.com>
2020-12-28 15:51:46 -0500

    leet code problem 108

--- problems.js
+++ problems.js
@@ -3382,3 +3382,13 @@ var canBeEqual = function (target, arr) {
   }
   return true;
 };
+
+var sortedArrayToBST = function (nums) {
+  if (nums === null || !nums.length) return null;
+
+  let mid = Math.floor(nums.length / 2);
+  let node = new TreeNode(nums[mid]);
+  node.left = sortedArrayToBST(nums.slice(0, mid));
+  node.right = sortedArrayToBST(nums.slice(mid + 1, nums.length));
+  return node
+};
