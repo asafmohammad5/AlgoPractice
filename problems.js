a1f4adb8d1aad4adbd96cbf3955fbd2f017f3cb7
asaf <asafmohammad5@gmail.com>
2020-12-30 13:21:05 -0500

    leet code problem 1534

--- problems.js
+++ problems.js
@@ -3525,3 +3525,17 @@ var toLowerCase = function (str) {
 var arrayStringsAreEqual = function (word1, word2) {
   return word1.reduce((acc, curr) => acc + curr) === word2.reduce((acc, curr) => acc + curr);
 };
+
+var countGoodTriplets = function (arr, a, b, c) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (Math.abs(arr[i] - arr[j]) > a) continue;
+      for (let k = j + 1; k < arr.length; k++) {
+        if (Math.abs(arr[j] - arr[k]) > b) continue;
+        if (Math.abs(arr[k] - arr[i]) <= c) result++;
+      }
+    }
+  }
+  return result;
+};
