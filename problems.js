3814824702a5286bc24ca06989845a3c2f862bbf
asaf <asafmohammad5@gmail.com>
2021-01-12 12:50:43 -0600

    AlgoExpert problem, Longest Peak

--- problems.js
+++ problems.js
@@ -4086,6 +4086,33 @@ function breaks(checker, prev, curr) {
   return differ > 0;
 }
 
+function longestPeak(array) {
+  let highest = 0;
+  let i = 0;
+
+  while (i < array.length - 1) {
+    const isPeak = array[i - 1] < array[i] && array[i + 1] < array[i];
+    if (!isPeak) {
+      i++;
+      continue;
+    }
+
+    let leftIdx = i - 2;
+    while (leftIdx >= 0 && array[leftIdx] < array[leftIdx + 1]) {
+      leftIdx--;
+    }
+
+    let rightIdx = i + 2;
+    while (rightIdx < array.length && array[rightIdx] < array[rightIdx - 1]) {
+      rightIdx++;
+    }
+    const currentPeak = rightIdx - leftIdx - 1;
+    highest = Math.max(highest, currentPeak);
+    i = rightIdx
+  }
+  return highest;
+}
+
 // Algo expert above
 
 
