1034f0b94190b3daec30456630da5713c4fb21de
asaf <asafmohammad5@gmail.com>
2021-01-12 14:57:10 -0600

    leetcode problem 406

--- problems.js
+++ problems.js
@@ -4252,6 +4252,7 @@ BrowserHistory.prototype.forward = function (steps) {
 };
 
 var spiralMatrixIII = function (R, C, r0, c0) {
+
   let n = Math.max(r0, c0, R - 1 - r0, C - 1 - c0);
   let result = [[r0, c0]];
 
@@ -4264,4 +4265,23 @@ var spiralMatrixIII = function (R, C, r0, c0) {
   }
 
   return result.filter(x => x[0] >= 0 && x[0] < R && x[1] >= 0 && x[1] < C);
+};
+
+var sortPeople = function (a, b) {
+  if (a[0] != b[0]) {
+    return b[0] - a[0];
+  } else {
+    return a[1] - b[1];
+  }
+}
+
+var reconstructQueue = function (people) {
+  people.sort(sortPeople);
+  var i;
+  var res = [];
+
+  for (i = 0; i < people.length; i++) {
+    res.splice(people[i][1], 0, people[i]);
+  }
+  return res;
 };
