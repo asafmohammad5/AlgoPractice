b5d00085153e12f3954e80fca6cb8f4dcc5ba200
asaf <asafmohammad5@gmail.com>
2021-01-12 14:20:55 -0600

    leetcode problem 885

--- problems.js
+++ problems.js
@@ -4250,3 +4250,18 @@ BrowserHistory.prototype.forward = function (steps) {
   }
   return this.history[this.incr - 1];
 };
+
+var spiralMatrixIII = function (R, C, r0, c0) {
+  let n = Math.max(r0, c0, R - 1 - r0, C - 1 - c0);
+  let result = [[r0, c0]];
+
+  for (let i = 1; i <= n; i++) {
+
+    for (let j = 1 - i; j <= i; j++) res.push([r0 + j, c0 + i]);
+    for (let j = i - 1; j >= -i; j--) res.push([r0 + i, c0 + j]);
+    for (let j = i - 1; j >= -i; j--) res.push([r0 + j, c0 - i]);
+    for (let j = 1 - i; j <= i; j++) res.push([r0 - i, c0 + j]);
+  }
+
+  return result.filter(x => x[0] >= 0 && x[0] < R && x[1] >= 0 && x[1] < C);
+};
