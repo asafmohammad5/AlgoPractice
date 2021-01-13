ea51207ea3aadc1ab6a924fc4b2433d2f9c2f7ae
asaf <asafmohammad5@gmail.com>
2021-01-13 14:33:46 -0600

    leetcode problem 889

--- problems.js
+++ problems.js
@@ -4346,3 +4346,13 @@ let xorQueries = (A, queries) => {
     ans.push(X[i] ^ X[j + 1]);
   return ans;
 };
+
+var constructFromPrePost = function (pre, post) {
+  if (pre.length === 0) return null;
+  if (pre.length === 1) return new TreeNode(pre[0]);
+  let res = new TreeNode(pre[0]);
+  let leftVal = pre[1], indexOfLeft = post.indexOf(leftVal);
+  res.left = constructFromPrePost(pre.slice(1, indexOfLeft + 2), post.slice(0, indexOfLeft + 1));
+  res.right = constructFromPrePost(pre.slice(indexOfLeft + 2), post.slice(indexOfLeft + 1));
+  return res;
+};
