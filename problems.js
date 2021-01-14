c13d79bf3f2f813918b0ad8ccab2697e705064ed
asaf <asafmohammad5@gmail.com>
2021-01-14 14:41:53 -0600

    AlgoExpert problem, Array of products

--- problems.js
+++ problems.js
@@ -4444,3 +4444,22 @@ let displayTable = orders => {
   })
   return res;
 };
+
+function arrayOfProducts(array) {
+  let results = new Array(array.length).fill(1);
+
+
+  let leftProd = 1;
+  for (let i = 0; i < array.length; i++) {
+    results[i] = leftProd;
+    leftProd *= array[i];
+  }
+
+  let rightProd = 1;
+  for (let i = array.length - 1; i >= 0; i--) {
+    results[i] *= rightProd;
+    rightProd *= array[i];
+  }
+  return results
+}
+
