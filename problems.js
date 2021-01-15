595341d8dd07404c8f39fdf0e12f68f749e2bba6
asaf <asafmohammad5@gmail.com>
2021-01-15 15:13:31 -0600

    leetcode problem 986

--- problems.js
+++ problems.js
@@ -4525,5 +4525,31 @@ function firstDuplicateValue(array) {
   return -1
 }
 
+var intervalIntersection = function (A, B) {
+  const results = [];
+
+  let i = 0;
+  let j = 0;
+
+  while (i < A.length && j < B.length) {
+    if (isOverlap(A[i], B[j])) {
+      const merged = [Math.max(A[i][0], B[j][0]), Math.min(A[i][1], B[j][1])];
+      results.push(merged);
+    }
+
+    if (A[i][1] < B[j][1]) {
+      i += 1;
+    } else {
+      j += 1;
+    }
+  }
+  return results;
+};
+
+function isOverlap([s1, e1], [s2, e2]) {
+
+  return (s2 <= e1 && e2 >= s1) || (s1 <= e2 && e1 >= s2);
+}
+
 
 
