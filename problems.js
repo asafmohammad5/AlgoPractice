bbd7802c00864e87a582fb87b65bb09369b09d2a
asaf <asafmohammad5@gmail.com>
2021-01-19 14:54:18 -0600

    leetcode problem 1512

--- problems.js
+++ problems.js
@@ -4712,4 +4712,15 @@ var minSetSize = function (arr) {
     if (count === length) return results;
   }
   return results;
-};ls
+};
+
+var numIdenticalPairs = function (nums) {
+  let count = 0;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] == nums[j]) count++
+    }
+  }
+  return count
+};
