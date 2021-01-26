d516e4ab07eedb4df5f0919ea30e5d169a4cb647
asaf <asafmohammad5@gmail.com>
2021-01-26 16:20:08 -0600

    leetcode problem 242

--- problems.js
+++ problems.js
@@ -4957,3 +4957,19 @@ var findTheDifference = function (s, t) {
   }
   return Object.keys(hash)[0]
 };
+
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) return false;
+  const hash = {};
+
+  for (let i = 0; i < s.length; i++) {
+    hash[s[i]] ? hash[s[i]]++ : hash[s[i]] = 1;
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    if (hash[t[i]]) hash[t[i]]--;
+    else return false;
+  }
+
+  return true;
+};
