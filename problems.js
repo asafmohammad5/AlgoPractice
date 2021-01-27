ca0bbccd66a5c8136c33a7708d2fdfb74ec540cd
asaf <asafmohammad5@gmail.com>
2021-01-27 14:56:22 -0600

    leetcode problem 1560

--- problems.js
+++ problems.js
@@ -5026,3 +5026,17 @@ const isValid = function (num) {
   return valid;
 };
 
+var mostVisited = function (n, rounds) {
+  let start = rounds[0];
+  let end = rounds[rounds.length - 1];
+  const result = [];
+
+  if (start <= end) {
+    for (let i = start; i <= end; i++) result.push(i);
+  } else {
+    for (let i = 1; i <= end; i++) result.push(i);
+    for (let i = start; i <= n; i++) result.push(i);
+  }
+  return result;
+};
+
