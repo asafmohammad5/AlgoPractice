ac2332e0e2fea55f63093004aaaeff7e3c97203d
asaf <asafmohammad5@gmail.com>
2021-01-27 16:03:54 -0600

    leetcode problem 217

--- problems.js
+++ problems.js
@@ -5040,3 +5040,8 @@ var mostVisited = function (n, rounds) {
   return result;
 };
 
+var containsDuplicate = function (nums) {
+  const set = new Set(nums);
+  return set.size < nums.length;
+};
+
