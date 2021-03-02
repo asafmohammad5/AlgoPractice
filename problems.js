363239b520b9cae874ce51db4f055dd48ed4bf7f
asaf <asafmohammad5@gmail.com>
2021-03-02 17:00:19 -0600

    leetcode problem 1758

--- problems.js
+++ problems.js
@@ -5741,6 +5741,32 @@ var canFormArray = function (arr, pieces) {
   return arr.every((x, i) => x == res[i]);
 };
 
+var minOperations = function (s) {
+  let count1 = 0;
+  let count2 = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    if (i % 2 === 0) {
+      if (s[i] === "0") {
+        count1++;
+      }
+      if (s[i] === "1") {
+        count2++;
+      }
+    }
+    if (i % 2 === 1) {
+      if (s[i] === "1") {
+        count1++;
+      }
+      if (s[i] === "0") {
+        count2++;
+      }
+    }
+  }
+  console.log([count1, count2])
+  return Math.min(count1, count2);
+};
+
 
 
 
