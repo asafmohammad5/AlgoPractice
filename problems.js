b6d734ccc8d5d015b951f6600e860c74c33f5eda
asaf <asafmohammad5@gmail.com>
2021-03-03 18:28:28 -0600

    leetcode problem 122

--- problems.js
+++ problems.js
@@ -5778,6 +5778,17 @@ var largestPerimeter = function (A) {
   return 0;
 };
 
+var maxProfit = function (prices) {
+  let profit = 0;
+
+  for (let i = 0; i < prices.length - 1; i++) {
+    if (prices[i] < prices[i + 1]) {
+      profit += (prices[i + 1] - prices[i])
+    }
+  }
+  return profit;
+};
+
 
 
 
