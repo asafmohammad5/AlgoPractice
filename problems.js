72805a2a3b71c07745dcd3aa35819ef740dd331d
asaf <asafmohammad5@gmail.com>
2021-03-25 14:46:00 -0500

    Did leetcode problem 100

--- problems.js
+++ problems.js
@@ -5884,6 +5884,12 @@ var countOdds = function (low, high) {
   return oddCount;
 };
 
+function isSameTree(p, q) {
+  if (!p && !q) return true;
+  if (!p || !q || p.val !== q.val) return false;
+
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+}
 
 
 
