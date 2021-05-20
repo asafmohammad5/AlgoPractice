f37ce2ab2ac2bfefd88546a249f4a390508bf7f2
asaf <asafmohammad5@gmail.com>
2021-05-20 16:30:19 -0500

    Did leetcode problem 1828

--- problems.js
+++ problems.js
@@ -5891,5 +5891,25 @@ function isSameTree(p, q) {
   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 }
 
+var countPoints = function (points, queries) {
+  const res = [];
+  for (let i = 0; i < queries.length; i++) {
+    let total = 0;
+    for (let j = 0; j < points.length; j++) {
+      if ((distance(points[j], queries[i]) <= queries[i][2])) {
+        total++;
+      }
+    }
+    res.push(total);
+  }
+  return res;
+};
+
+function distance(point1, point2) {
+  const [x1, y1] = point1;
+  const [x2, y2] = point2;
+  return Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+}
+
 
 
