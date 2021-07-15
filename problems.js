b2ac221cf0a5473a88fcb8f97bfc21ed54337656
asaf <asafmohammad5@gmail.com>
2021-07-15 17:37:48 -0500

    Did leetcode problem 1652

--- problems.js
+++ problems.js
@@ -5977,3 +5977,21 @@ const findCenter = (edges) => {
 // ###### RUBY ######
 
 
+const decrypt = (code, k) => {
+
+  const decode = (code, k) => {
+    const result = [];
+    let sum = 0;
+    for (let i = 0; i < k; i++)
+      sum += code[i];
+
+    for (let i = 0; i < code.length; i++) {
+      sum -= code[i];
+      sum += code[(i + k) % code.length];
+      result.push(sum);
+    }
+    return result;
+  }
+
+  return k > 0 ? decode(code, k) : decode(code.reverse(), -k).reverse();
+};
