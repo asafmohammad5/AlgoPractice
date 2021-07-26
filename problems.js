04f27d4692b9a87da06ede9ac8b5ab641a9395e5
asaf <asafmohammad5@gmail.com>
2021-07-26 13:12:39 -0500

    Did leetcode problem 27

--- problems.js
+++ problems.js
@@ -6005,3 +6005,14 @@ var isUnivalTree = function (root) {
   };
   return trav(root);
 };
+
+var removeElement = function (nums, val) {
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+  return i;
+};
